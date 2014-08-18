26f983aa122c38bfa45d1ced8e496e3530c5edff
McIvor Steiner <mcivorsteiner@gmail.com>
2014-08-17 21:44:46 -0700

    created new SpotifyAuth class and moved code fromcontroller to this class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,60 +2,36 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    spotify_client = Spotify::Client.new(access_token: session[:access_token])
+    spotify_client = SpotifyClient.new(access_token: session[:access_token])
     @artists = spotify_client.get_user_artists
   end
 
   def create
-    client_id = ENV['CLIENT_ID']
-    client_secret = ENV['CLIENT_SECRET']
-    client_callback_url = "http://localhost:3000/callback/"
-    # AUTH_HEADER = "Basic " + Base64.strict_encode64(CLIENT_ID + ":" + CLIENT_SECRET)
-    # SPOTIFY_ACCOUNTS_ENDPOINT = URI.parse("https://accounts.spotify.com")
-    # SPOTIFY_PROFILE_ENDPOINT = URI.parse("https://api.spotify.com")
-
-
-    state_key = SecureRandom.urlsafe_base64
-    session[:spotify_auth_state] = state_key
-    scope = "user-read-private user-read-email user-library-read playlist-read-private"
-    query_hash = {
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: client_callback_url,
-      state: state_key
-    }
-    url = 'https://accounts.spotify.com/authorize?' + query_hash.to_query
-    redirect_to url
+    auth_client = SpotifyAuth.new
+    state = SecureRandom.urlsafe_base64
+    session[:spotify_auth_state] = state
+    redirect_to auth_client.spotify_auth_url(state)
   end
 
   def callback
-    client_callback_url = "http://localhost:3000/callback/"
-    client_id = ENV['CLIENT_ID']
-    client_secret = ENV['CLIENT_SECRET']
     auth_code = params[:code]
-    state = params[:state]
+    auth_client = SpotifyAuth.new
 
-    if state == session[:spotify_auth_state]
+    if params[:state] == session[:spotify_auth_state]
       session.clear
-      query_hash = {
-        code: auth_code,
-        redirect_uri: client_callback_url,
-        grant_type: 'authorization_code',
-        client_id: client_id,
-        client_secret: client_secret
-      }
-      response = HTTParty.post('https://accounts.spotify.com/api/token', query: query_hash)
+      response = auth_client.request_tokens(auth_code)
 
       if response.code.to_i == 200
         token_data = JSON.parse(response.body)
-        spotify_client = Spotify::Client.new(access_token: token_data["access_token"])
+        spotify_client = SpotifyClient.new(access_token: token_data["access_token"])
         profile_data = spotify_client.get_profile_data
+
         if @user = User.find_by_spotify_user_id(profile_data["id"])
           @user.update_attributes( refresh_token: token_data["refresh_token"])
         else
           @user = User.create( spotify_user_id: profile_data["id"], refresh_token: token_data["refresh_token"])
         end
+
         session[:id] = @user.id
         session[:access_token] = token_data["access_token"]
         redirect_to @user
@@ -64,5 +40,4 @@ class UsersController < ApplicationController
       redirect_to root
     end
   end
-
 end
---
+++ app/models/spotify_auth.rb
@@ -0,0 +1,35 @@
+class SpotifyAuth
+  include HTTParty
+  base_uri 'https://accounts.spotify.com'
+
+  def initialize
+    @client_id = ENV['CLIENT_ID']
+    @client_secret = ENV['CLIENT_SECRET']
+    @redirect_uri = "http://localhost:3000/callback/"
+    @scope = "user-read-private user-read-email user-library-read playlist-read-private"
+  end
+
+  def spotify_auth_url(state)
+    query_hash = {
+      response_type: 'code',
+      client_id: @client_id,
+      scope: @scope,
+      redirect_uri: @redirect_uri,
+      state: state
+    }
+
+    'https://accounts.spotify.com/authorize?' + query_hash.to_query
+  end
+
+  def request_tokens(auth_code)
+    query_hash = {
+      grant_type: 'authorization_code',
+      code: auth_code,
+      redirect_uri: @redirect_uri,
+      client_id: @client_id,
+      client_secret: @client_secret
+    }
+
+    self.class.post('/api/token', query: query_hash)
+  end
+end
