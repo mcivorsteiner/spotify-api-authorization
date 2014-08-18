9ef35ca3b543469e8e96c2125eb7fb512253dcee
McIvor Steiner <mcivorsteiner@gmail.com>
2014-08-17 20:09:14 -0700

    created a spotify class for api calls and moved methods from controller into new class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find_by_spotify_user_id(session[:spotify_user_id])
-    @tracks = get_user_tracks(session[:access_token])
-    @artists = aggregate_artists(@tracks)
+    @user = User.find(params[:id])
+    spotify_client = Spotify::Client.new(access_token: session[:access_token])
+    @artists = spotify_client.get_user_artists
   end
 
   def create
@@ -49,75 +49,20 @@ class UsersController < ApplicationController
 
       if response.code.to_i == 200
         token_data = JSON.parse(response.body)
-        profile_data = get_profile_data(token_data["access_token"])
+        spotify_client = Spotify::Client.new(access_token: token_data["access_token"])
+        profile_data = spotify_client.get_profile_data
         if @user = User.find_by_spotify_user_id(profile_data["id"])
           @user.update_attributes( refresh_token: token_data["refresh_token"])
         else
           @user = User.create( spotify_user_id: profile_data["id"], refresh_token: token_data["refresh_token"])
         end
-        session[:spotify_user_id] = profile_data["id"]
+        session[:id] = @user.id
         session[:access_token] = token_data["access_token"]
         redirect_to @user
       end
     else
       redirect_to root
     end
-  end
-
-  private
-
-  def get_profile_data(access_token)
-    spotify_profile_endpoint = URI.parse("https://api.spotify.com")
-    
-    http = Net::HTTP.new(spotify_profile_endpoint.host, spotify_profile_endpoint.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Get.new("/v1/me")
-    request.add_field("Authorization", "Bearer " + access_token)
-    response = http.request(request)
-
-    return JSON.parse(response.body)
-  end
-
-  def get_user_tracks(access_token)
-    tracks = { "items" => [] }
-
-    spotify_profile_endpoint = URI.parse("https://api.spotify.com")
-    
-    http = Net::HTTP.new(spotify_profile_endpoint.host, spotify_profile_endpoint.port)
-    http.use_ssl = true
-
-    path = "/v1/me/tracks"
-
-    while path
-      request = Net::HTTP::Get.new(path)
-      request.add_field("Authorization", "Bearer " + access_token)
-      response = http.request(request)
-      parsed_response = JSON.parse(response.body)
-      tracks["items"].concat(parsed_response.delete("items"))
-      tracks.merge!(parsed_response)
-      path = if parsed_response["next"]
-        parsed_response["next"].gsub("https://api.spotify.com", '')
-      else
-        nil
-      end
-    end
-
-    return tracks
-  end
-
-  def aggregate_artists(tracks)
-    top_artists = {}
-    artist_cache = {}
-    tracks["items"].each do |item|
-      artist = item["track"]["artists"].first
-      unless top_artists.has_key?(artist["uri"])
-        top_artists[artist["uri"]] = 0
-        artist_cache[artist["uri"]] = artist
-      end
-      top_artists[artist["uri"]] += 1
-    end
-    { top_artists: top_artists, artist_cache: artist_cache }
   end
 
 end
---
+++ app/models/spotify.rb
@@ -0,0 +1,56 @@
+module Spotify
+  class Client
+    include HTTParty
+    base_uri 'https://api.spotify.com'
+
+    attr_accessor :access_token
+
+    def initialize(args = {})
+      @access_token = args[:access_token]
+    end
+
+    def request(path, options = {})
+      response = self.class.get(path, options)
+      JSON.parse(response.body)
+    end
+
+    def get_profile_data
+      request('/v1/me', headers: { "Authorization" => "Bearer #{@access_token}" })
+    end
+
+    def get_user_tracks
+      tracks = { "items" => [] }
+      path = "/v1/me/tracks"
+
+      while path
+        response = request(path, headers: { "Authorization" => "Bearer #{@access_token}" })
+        tracks["items"].concat(response.delete("items"))
+        tracks.merge!(response)
+        path = response["next"] ? response["next"].gsub("https://api.spotify.com", "") : nil
+      end
+
+      tracks
+    end
+
+    def get_user_artists
+      tracks = get_user_tracks
+      aggregate_artists(tracks)
+    end
+
+    private
+
+    def aggregate_artists(tracks)
+      top_artists = {}
+      artist_cache = {}
+      tracks["items"].each do |item|
+        artist = item["track"]["artists"].first
+        unless top_artists.has_key?(artist["uri"])
+          top_artists[artist["uri"]] = 0
+          artist_cache[artist["uri"]] = artist
+        end
+        top_artists[artist["uri"]] += 1
+      end
+      { top_artists: top_artists, artist_cache: artist_cache }
+    end
+  end
+end
